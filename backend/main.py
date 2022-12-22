b0a11a3bb01b72b98ab8a89b087cbf2b6ab06c9e
Zach Parks <zach@alliware.com>
2022-12-22 15:14:11 -0600

    add AP as dependency

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,17 @@
 import uvicorn
-
+from os import path
+import sys
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+    # uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+
+    # TODO: For testing purposes, we're just trying to get data.
+    sys.path.append(path.abspath("./archipelago"))
+    from archipelago.Generate import roll_settings
+    return_val = roll_settings({
+        "name": "Phar",
+        "description": "Test",
+        "game": "Archipelago",
+        "Archipelago": {},
+    })
+    print(return_val)
